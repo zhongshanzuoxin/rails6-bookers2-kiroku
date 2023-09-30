3f64de189cd43e3d7379f0167556792e85fe6c8f
takuminakayama <143808505+zhongshanzuoxin@users.noreply.github.com>
2023-09-30 00:56:40 +0000

    [Finish]4-7

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,66 @@
+class BooksController < ApplicationController
+  before_action :correct_user, only: [:update, :edit, :destroy]
+
+  before_action :move_to_signed_in
+
+
+  def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+    @book_comment = BookComment.new
+  end
+
+  def index
+    @books = Book.all
+    @user = current_user
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book), notice: "You have created book successfully."
+    else
+      redirect_to books_path, flash: { error: @book.errors.full_messages }
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: "You have updated book successfully."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+
+  def move_to_signed_in
+    unless user_signed_in?
+      redirect_to  '/users/sign_in'
+    end
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
+
+
+def correct_user
+  @book = Book.find(params[:id])
+  @user = @book.user
+  redirect_to(books_path) unless @user == current_user
+end
---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root to: "homes#top"
+  get "home/about"=>"homes#about"
+  get "search" => "searches#search"
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
+  resources :users, only: [:index,:show,:edit,:update] do
+    member do
+      get :follows, :followers
+    end
+      resource :relationships, only: [:create, :destroy]
+  end
+end
